81acfec37cd03287d748316c101262fd63645754
Martinmg10 <mg100martingonzalez@gmail.com>
2023-03-31 03:08:35 -0300

    se agrega la tarea completada

---
+++ app.js
@@ -0,0 +1,58 @@
+// 1- A través de document.write concatena un saludo almacenado en 2 variables.
+
+
+let saludo1 = 'Hola ';
+let saludo2 = 'soy Martin';
+
+document.write (saludo1 + saludo2);
+
+//Ejercicio 2
+
+let numero1 = 10
+let numero2 = 5
+
+
+//suma
+console.log('SUMA')
+let suma = numero1 + numero2
+console.log(suma)
+
+
+//resta
+console.log('RESTA')
+let resta = numero1 - numero2
+console.log(resta)
+
+
+//producto
+console.log('PRODUCTO')
+ let producto = numero1 * numero2
+console.log(producto)
+
+
+//division
+console.log('DIVISION')
+let division = numero1 / numero2
+console.log(division)
+
+
+// par o impar
+console.log('par o impar')
+
+let parimpar = numero2 % 2 == 0 ? 'es par' :'es impar'
+console.log(parimpar)
+    
+//ejercicio 3
+console.log('EJERCICIO 3')
+
+let numero3 = 15
+
+let suma2 = suma + numero3
+console.log(suma2)
+
+let resta2 = resta - numero3
+console.log(resta2)
+
+let producto2 = producto * numero3
+console.log(producto2)
+
